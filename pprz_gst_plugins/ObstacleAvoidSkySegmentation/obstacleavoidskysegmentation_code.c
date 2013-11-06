ad9d3708a6e775fd21c7675d6d0480c6e2443a91
Christophe De Wagter <dewagter@gmail.com>
2013-11-06 17:03:26 +0100

    New Plugin

---
+++ pprz_gst_plugins/ObstacleAvoidSkySegmentation/obstacleavoidskysegmentation_code.c
@@ -0,0 +1,17 @@
+
+#include "obstacleavoidskysegmentation_code.h"
+
+// Settable by pluging
+unsigned int imgWidth, imgHeight;
+unsigned int tcpport;
+unsigned char yourownvariable;
+
+// Called by plugin
+void my_plugin_init(void)
+{
+}
+
+void my_plugin_run(unsigned char *frame)
+{
+}
+

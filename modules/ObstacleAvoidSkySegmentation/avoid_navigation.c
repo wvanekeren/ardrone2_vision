383c0edf8da30d3550c61f336c7b7b64f0769ae7
Christophe De Wagter <dewagter@gmail.com>
2013-11-28 16:40:37 +0100

    avoid navigation

--- modules/ObstacleAvoidSkySegmentation/avoid_navigation.c
+++ modules/ObstacleAvoidSkySegmentation/avoid_navigation.c
@@ -1,31 +1,38 @@
 /*
- * Copyright (C) 2008-2010 The Paparazzi Team
+ * Copyright (C) 2012-2013
  *
- * This file is part of paparazzi.
+ * This file is part of Paparazzi.
  *
- * paparazzi is free software; you can redistribute it and/or modify
+ * Paparazzi is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2, or (at your option)
  * any later version.
  *
- * paparazzi is distributed in the hope that it will be useful,
+ * Paparazzi is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with paparazzi; see the file COPYING.  If not, write to
+ * along with Paparazzi; see the file COPYING.  If not, write to
  * the Free Software Foundation, 59 Temple Place - Suite 330,
  * Boston, MA 02111-1307, USA.
  */
 
-/**
- * @file video.c
- * Video processor access function
- */
+// Own Header
+#include "avoid_navigation.h"
+
+// Paparazzi Data
+#include "subsystems/gps/gps_ardrone2.h"
+#include "subsystems/imu/imu_ardrone2_raw.h"
 
 
-#include "video.h"
+void init_avoid_navigation()
+{
 
-struct Video video;
+}
 
+void run_avoid_navigation_onvision()
+{
+
+}
